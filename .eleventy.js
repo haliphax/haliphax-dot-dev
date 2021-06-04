12a70392bf088ce06cecff61633930add5fd7aae
haliphax <haliphax@gmail.com>
2021-06-04 15:48:55 -0500

    fix 404 ref for browserSync

--- .eleventy.js
+++ .eleventy.js
@@ -14,7 +14,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.setBrowserSyncConfig({
 		callbacks: {
 			ready(err, browserSync) {
-				const content_404 = fs.readFileSync('_site/404.html');
+				const content_404 = fs.readFileSync('docs/404.html');
 
 				browserSync.addMiddleware('*', (req, res) => {
 					// Provides the 404 content without redirect.
