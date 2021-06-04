e39f5d1327621f0ee46486a1854803744ffe663b
haliphax <haliphax@gmail.com>
2021-06-04 13:31:26 -0500

    move tag rendering to own file and function

--- _includes/layouts/post.11ty.js
+++ _includes/layouts/post.11ty.js
@@ -1,4 +1,4 @@
-const ignoreTags = require('../ignoreTags');
+const renderTags = require('../renderTags');
 
 module.exports = class Post {
 	get data() {
@@ -8,25 +8,8 @@ module.exports = class Post {
 	}
 
 	render(data) {
-		const tags = data.tags.filter(t => ignoreTags.indexOf(t) < 0);
-
 		return /*html*/`
-			<span class="sr-only">Tags:</span>
-
-			${tags.length == 0 ? '' : /*html*/`
-				<ul class="list-unstyled d-inline-block mb-10">
-					${tags.map(t => /*html*/`
-							<li class="d-inline-block">
-								<a href="/tags/${t}/"
-									class="badge badge-secondary">
-									<span class="fas fa-tag"></span>
-									${t}
-								</a>
-							</li>
-						`)
-						.join('')}
-				</ul>`}
-
+			${renderTags(data.tags)}
 			<div class="card border mx-0 pb-0 pt-5 mt-0">
 				${data.content}
 			</div>
