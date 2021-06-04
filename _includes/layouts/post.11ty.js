32c082215592be6f5d875db35043346039b332ff
haliphax <haliphax@gmail.com>
2021-06-04 13:24:42 -0500

    hide tags list if post has no tags

--- _includes/layouts/post.11ty.js
+++ _includes/layouts/post.11ty.js
@@ -8,20 +8,25 @@ module.exports = class Post {
 	}
 
 	render(data) {
+		const tags = data.tags.filter(t => ignoreTags.indexOf(t) < 0);
+
 		return /*html*/`
 			<span class="sr-only">Tags:</span>
-			<ul class="list-unstyled d-inline-block mb-10">
-				${data.tags.filter(t => ignoreTags.indexOf(t) < 0)
-					.map(t => /*html*/`
-						<li class="d-inline-block">
-							<a href="/tags/${t}/"
-								class="badge badge-secondary">
-								<span class="fas fa-tag"></span>
-								${t}
-							</a>
-						</li>
-					`).join('')}
-			</ul>
+
+			${tags.length == 0 ? '' : /*html*/`
+				<ul class="list-unstyled d-inline-block mb-10">
+					${tags.map(t => /*html*/`
+							<li class="d-inline-block">
+								<a href="/tags/${t}/"
+									class="badge badge-secondary">
+									<span class="fas fa-tag"></span>
+									${t}
+								</a>
+							</li>
+						`)
+						.join('')}
+				</ul>`}
+
 			<div class="card border mx-0 pb-0 pt-5 mt-0">
 				${data.content}
 			</div>
