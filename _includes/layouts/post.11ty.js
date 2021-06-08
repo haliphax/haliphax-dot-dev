b463aeeaf7314a5198812657688cdf34e123400d
haliphax <haliphax@gmail.com>
2021-06-08 01:53:48 -0500

    post permalinks

--- _includes/layouts/post.11ty.js
+++ _includes/layouts/post.11ty.js
@@ -2,6 +2,15 @@ module.exports = class Post {
 	get data() {
 		return {
 			layout: 'withHeader',
+			permalink(data) {
+				const y = this.page.date.getFullYear(),
+					m = this.page.date.getMonth() + 1,
+					m0 = m < 10 ? `0${m}` : m,
+					d = this.page.date.getDate(),
+					d0 = d < 10 ? `0${d}` : d;
+
+				return `/${y}/${m0}/${d0}/${this.page.fileSlug}/`;
+			},
 		};
 	}
 
