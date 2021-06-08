0c4f461a9cc6ca149351d52df4611b32b3792cab
haliphax <haliphax@gmail.com>
2021-06-08 01:58:56 -0500

    tags padding

--- content/tags.11ty.js
+++ content/tags.11ty.js
@@ -9,13 +9,14 @@ module.exports = class Tags {
 
 	render(data) {
 		const tags = Object.keys(data.collections)
-			.filter(t => data.ignoreTags.indexOf(t) < 0);
+			.filter(t => data.ignoreTags.indexOf(t) < 0)
+			.sort((a, b) => a.localeCompare(b));
 
 		return /*html*/`
 			<div class="card border mt-0 mx-0 pt-20 pb-0">
 				<ul class="list-unstyled">
 					${tags.map(t => /*html*/`
-						<li class="d-inline-block mr-5 mb-5">
+						<li class="d-inline-block mr-5 mb-10">
 							<a href="/tags/${t}/" class="btn btn-secondary">
 								<span class="fas fa-tag"></span>
 								${t}
