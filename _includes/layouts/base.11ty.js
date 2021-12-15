c63d9a08358ab7827c1b60afa32d0a15d61994fe
haliphax <haliphax@gmail.com>
2021-12-15 12:32:25 -0600

    specific tags for twitter metadata

--- _includes/layouts/base.11ty.js
+++ _includes/layouts/base.11ty.js
@@ -36,13 +36,15 @@ module.exports = class Base {
 					<meta charset="utf-8" />
 					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 					<title>${data.title} | ${data.strings.siteName}</title>
+					<meta property="og:title" content="${ogTitle}" />
+					<meta property="og:description" content="${metaDescription}" />
+					<meta property="og:image" content="${ogImage}" />
 					<meta name="twitter:card" content="summary_large_image" />
 					<meta name="twitter:site" content="${data.strings.twitter}" />
 					<meta name="twitter:creator" content="${data.strings.twitter}" />
-					<meta name="description" content="${metaDescription}" />
-					<meta property="og:description" content="${metaDescription}" />
-					<meta property="og:image" content="${ogImage}" />
-					<meta property="og:title" content="${ogTitle}" />
+					<meta name="twitter:title" content="${ogTitle}" />
+					<meta name="twitter:description" content="${metaDescription}" />
+					<meta name="twitter:image" content="${ogImage}" />
 					<link rel="icon" href="/img/favicon.gif" />
 					<link href="https://cdn.jsdelivr.net/npm/halfmoon@1.1.1/css/halfmoon-variables.min.css" rel="stylesheet" media="screen" />
 					<link href="/css/styles.css" rel="stylesheet" />
