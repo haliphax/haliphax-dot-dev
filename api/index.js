8f218c90e023c96f1327c1def7a0da35a928532f
haliphax <haliphax@gmail.com>
2022-03-27 01:00:31 -0500

    new fetch require pattern

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const jsonDiff = require('json-diff');
 const TwitchApi = require('node-twitch').default;
 
