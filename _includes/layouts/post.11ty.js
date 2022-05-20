dd06620b332a15efdf4d048a7a5784a6e60a4036
haliphax <haliphax@gmail.com>
2022-05-20 12:47:56 -0500

    leave room in meta description length for ellipsis

--- _includes/layouts/post.11ty.js
+++ _includes/layouts/post.11ty.js
@@ -14,7 +14,7 @@ module.exports = class Post {
 						.replace(/<[^>]+>/g, '')
 						.replace(/\s{2,}/g, ' ')
 						.replace(/\n/g, ' '))
-						.slice(0, 160)
+						.slice(0, 159)
 						.replace(/\s+[^ ]*$/, '') + ' &hellip;';
 				},
 			},
