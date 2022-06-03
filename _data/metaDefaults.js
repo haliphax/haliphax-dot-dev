6f02d018dda5d287736fb9043abdc586116466bd
haliphax <haliphax@gmail.com>
2022-06-03 10:24:03 -0500

    combine ogImage tags; refactor data files

--- _data/metaDefaults.js
+++ _data/metaDefaults.js
@@ -4,5 +4,6 @@ module.exports = {
 	author: 'haliphax',
 	description: 'haliphax streams software development on Twitch.',
 	generator: `${eleventyPackage.name} v${eleventyPackage.version}`,
+	openGraphImageUrl: 'https://res.cloudinary.com/haliphax/image/upload/c_scale,e_blur:150,q_auto,w_1200,h_600/fl_no_overflow,c_scale,g_north_west,l_haliphax.dev:hx-transparent_y2yati,r_0,w_150,x_20,y_20/bo_8px_solid_rgb:000000,c_scale,g_center,l_text:shrikhand_96_stroke_center:{title},co_white,c_fit,fl_text_no_trim,fl_text_disallow_overflow,w_600,w_1200/v1639552617/haliphax.dev/stream-bg-2_mdc1sc.jpg',
 	openGraphType: 'article',
 };
--- _includes/layouts/base.11ty.js
+++ _includes/layouts/base.11ty.js
@@ -32,7 +32,7 @@ module.exports = class Base {
 		const metaDescription = metaEncode(
 			data.metaDescription ?? data.metaDefaults.description);
 		const metaLabels = [];
-		const ogImage = data.strings.openGraphImageUrl.replace(
+		const ogImage = data.metaDefaults.openGraphImageUrl.replace(
 			'{title}', encodeURIComponent(data.ogTitle ?? data.title));
 		const canonicalUrl = `${data.strings.siteRoot}${this.page.url}`;
 
@@ -57,7 +57,7 @@ module.exports = class Base {
 					<meta property="og:image:height" content="600" />
 					<meta property="og:image:type" content="image/jpeg" />
 					<meta property="og:image:width" content="1200" />
-					<meta property="og:image" content="${ogImage}" />
+					<meta property="og:image" name="twitter:image" content="${ogImage}" />
 					<meta property="og:site_name" content="${data.strings.siteName}" />
 					<meta property="og:title" content="${ogTitle}" />
 					<meta property="og:type" content="${ogType}" />
@@ -71,7 +71,6 @@ module.exports = class Base {
 					<meta name="twitter:card" content="summary_large_image" />
 					<meta name="twitter:creator" content="${data.strings.twitter}" />
 					<meta name="twitter:description" content="${metaDescription}" />
-					<meta name="twitter:image" content="${ogImage}" />
 					<meta name="twitter:site" content="${data.strings.twitter}" />
 					<meta name="twitter:title" content="${ogTitle}" />
 					${metaLabels.length === 0 ? ''
