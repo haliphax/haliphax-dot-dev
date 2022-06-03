84475ffe3eb1618e4f3c8fb77fddc9e56f1cf004
haliphax <haliphax@gmail.com>
2022-06-03 10:08:03 -0500

    generator metadata tag

--- _data/metaDefaults.js
+++ _data/metaDefaults.js
@@ -1,5 +1,8 @@
+const eleventyPackage = require('@11ty/eleventy/package.json');
+
 module.exports = {
 	author: 'haliphax',
 	description: 'haliphax streams software development on Twitch.',
+	generator: `${eleventyPackage.name} v${eleventyPackage.version}`,
 	openGraphType: 'article',
 };
