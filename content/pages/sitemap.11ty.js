64537e8a15e18e42792b324d09c19090620e3d15
haliphax <haliphax@gmail.com>
2022-06-09 00:55:12 -0500

    indendation

--- content/pages/sitemap.11ty.js
+++ content/pages/sitemap.11ty.js
@@ -11,15 +11,15 @@ module.exports = class SiteMap {
 			<?xml version="1.0" encoding="utf-8"?>
 			<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 				${data.collections.all
-			.filter(page => !page.data.draft
-				&& !(page.data.robots ?? '').includes('noindex'))
-			.map(page => /*xml*/`
-					<url>
-						<loc>${data.strings.siteRoot}${page.url}</loc>
-						<lastmod>${page.date.toISOString()}</lastmod>
-						<changefreq>${page.data.changeFreq ? page.data.changeFreq : 'never'}</changefreq>
-					</url>
-					`).join('')}
+					.filter(page => !page.data.draft
+						&& !(page.data.robots ?? '').includes('noindex'))
+					.map(page => /*xml*/`
+						<url>
+							<loc>${data.strings.siteRoot}${page.url}</loc>
+							<lastmod>${page.date.toISOString()}</lastmod>
+							<changefreq>${page.data.changeFreq ? page.data.changeFreq : 'never'}</changefreq>
+						</url>
+						`).join('')}
 			</urlset>
 			`.trim();
 	}
