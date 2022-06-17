38f4135fda46b91c61198f0b6afb58bd44f5866c
haliphax <haliphax@gmail.com>
2022-06-16 23:48:56 -0500

    reword intro blurb

--- content/pages/index.11ty.js
+++ content/pages/index.11ty.js
@@ -25,8 +25,8 @@ module.exports = class Index {
 						I am a software engineer and
 						<a href="https://oddnetwork.org/ascii/">textmode artist</a>. I
 						worked as a web developer in higher education for almost 15 years,
-						but I have recently moved into the private sector to work with
-						microservices and cloud native solutions. I enjoy solving problems
+						but I have recently moved into the private sector to build
+						microservices and cloud-native solutions. I enjoy solving problems
 						and sharing what I have learned with others.
 					</div>
 				</div>
