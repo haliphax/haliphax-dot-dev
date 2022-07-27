c47f1adb625350a8453f58fcde3317efa62599cc
haliphax <haliphax@users.noreply.github.com>
2022-07-26 22:09:13 -0500

    remove vestigial "collection" layout

--- 11ty/layouts/_config.js
+++ 11ty/layouts/_config.js
@@ -1,6 +1,5 @@
 const config = cfg => {
 	cfg.addLayoutAlias('base', 'layouts/base.11ty.js');
-	cfg.addLayoutAlias('collection', 'layouts/collection.11ty.js');
 	cfg.addLayoutAlias('post', 'layouts/post.11ty.js');
 	cfg.addLayoutAlias('withHeader', 'layouts/withHeader.11ty.js');
 }
