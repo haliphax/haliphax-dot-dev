f2650b75a06e533dbb8de0ac60775ffcaa517c1b
haliphax <haliphax@users.noreply.github.com>
2023-07-19 11:55:48 -0500

    semicolon

--- 11ty/functions/getContent.js
+++ 11ty/functions/getContent.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 const getContent = data => fs.readFileSync(data.page.inputPath, 'utf8')
 	.replace(/^(.|\n)+?---\n/m, '');
 
-module.exports = getContent
+module.exports = getContent;
