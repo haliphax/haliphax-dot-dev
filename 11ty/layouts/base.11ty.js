2d21b36cfdd7f9ea36401d59b6b62d5cc204dea0
haliphax <haliphax@gmail.com>
2023-07-21 11:23:47 -0500

    fix details/summary fade

--- 11ty/layouts/base.11ty.js
+++ 11ty/layouts/base.11ty.js
@@ -142,9 +142,9 @@ module.exports = class Base {
 							</div>
 						</div>
 					</div>
-					<script id="sidebar-script">
+					<script id="global-scripts">
+						// sidebar management
 						(() => {
-							// Sidebar management
 							const wrapper = document.querySelector('.page-wrapper');
 							const dsh = 'data-sidebar-hidden';
 
@@ -156,6 +156,47 @@ module.exports = class Base {
 									wrapper.getAttribute(dsh)
 										? wrapper.removeAttribute(dsh)
 										: wrapper.setAttribute(dsh, 'true'));
+						})();
+
+						// <details> fade in/out
+						(() => {
+							// set initial delay to low value to correct for layout bug :(
+							let delay = 1;
+
+							// manage fade-out of details content
+							Array.from(document.querySelectorAll('summary')).map(s => {
+								const d = s.parentNode;
+
+								const close = () => {
+									d.classList.remove('closing');
+									s.dataset.close = '';
+									d.open = null;
+								};
+
+								const click = e => {
+									if (!d.open) return true;
+
+									if (d.open && s.dataset.close !== '1') {
+										setTimeout(close, delay);
+										d.classList.add('closing');
+										s.dataset.close = '1';
+									}
+
+									e.preventDefault();
+									return false;
+								};
+
+								s.addEventListener('click', click);
+
+								// layout bug fix
+								d.classList.add('loading');
+								s.click();
+								requestAnimationFrame(() => {
+									s.click();
+									d.classList.remove('loading');
+									delay = 500;
+								});
+							});
 						})();
 					</script>
 				</body>
