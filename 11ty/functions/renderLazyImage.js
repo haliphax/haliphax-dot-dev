1b10be822ca6c865d42ab7fe44c257e02f87d9c6
haliphax <haliphax@gmail.com>
2023-07-24 14:54:14 -0500

    use inline transparent base64 pixel for lazy image placeholder

--- 11ty/functions/renderLazyImage.js
+++ 11ty/functions/renderLazyImage.js
@@ -1,6 +1,12 @@
-const renderLazyImage = html => /*html*/`
-	${html.replace(/(\s|^)src=/ig, '$1data-src=')}
-	<noscript>${html}</noscript>
-`;
+const base64Pixel = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+
+const renderLazyImage = html => {
+	const swapped = html.replace(
+		/(\s|^)src=/ig,
+		`$1src="data:image/png;base64,${base64Pixel}" data-src=`
+	);
+
+	return /*html*/`${swapped}<noscript>${html}</noscript>`;
+};
 
 module.exports = renderLazyImage;
