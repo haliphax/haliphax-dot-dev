2e6b358a30280b550133ea9fbbcd5f256b95a7de
haliphax <haliphax@users.noreply.github.com>
2023-07-26 11:51:27 -0500

    sitemap tweaks

--- content/pages/sitemap.11ty.js
+++ content/pages/sitemap.11ty.js
@@ -11,13 +11,12 @@ module.exports = class SiteMap {
 			<?xml version="1.0" encoding="utf-8"?>
 			<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 				${data.collections.all
-					.filter(page => !page.data.draft
-						&& !(page.data.robots ?? '').includes('noindex'))
+					.filter(page => !page.data.draft && !page.data.robots?.includes('noindex'))
 					.map(page => /*xml*/`
 						<url>
 							<loc>${data.strings.siteRoot}${page.url}</loc>
 							<lastmod>${page.date.toISOString()}</lastmod>
-							<changefreq>${page.data.changeFreq ? page.data.changeFreq : 'never'}</changefreq>
+							<changefreq>${page.data.changeFreq ?? 'never'}</changefreq>
 						</url>
 						`).join('')}
 			</urlset>
