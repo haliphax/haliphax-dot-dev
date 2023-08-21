3473400c2f7c1a0a1e932e9238e93f5188794484
haliphax <haliphax@gmail.com>
2023-08-21 16:46:56 -0500

    fix loaded behavior

--- 11ty/layouts/base/loaded.ts
+++ 11ty/layouts/base/loaded.ts
@@ -1,2 +1,2 @@
 // loaded indicator for use in styles
-document.addEventListener("load", () => document.body.classList.add("x"));
+window.addEventListener("load", () => document.body.classList.add("x"));
